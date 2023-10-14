b70f1475538ac5601d3171ff1335f0d05b186c0d
RizwanRumi <rizwan.cse33@gmail.com>
2023-10-14 11:23:48 +0200

    add initial project files

---
+++ dSPACE.Programming.Task/dSPACE.Programming.Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dSPACE.Programming.Task
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("dSAPCE Programming Task Solution:");
+        }
+    }
+}
