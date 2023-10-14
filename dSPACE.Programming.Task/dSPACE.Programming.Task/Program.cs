e6c5a697adfb3c87d72113580281cada9ce41d89
RizwanRumi <rizwan.cse33@gmail.com>
2023-10-14 11:50:41 +0200

    add SignalDecoder class file

--- dSPACE.Programming.Task/dSPACE.Programming.Task/Program.cs
+++ dSPACE.Programming.Task/dSPACE.Programming.Task/Program.cs
@@ -3,10 +3,13 @@
 namespace dSPACE.Programming.Task
 {
     internal class Program
-    {        
+    {
+        private static SignalDecoder signalDecoder = null;
         static void Main(string[] args)
-        {            
-            Console.WriteLine("dSAPCE Programming Task Solution:");
+        {
+            Console.WriteLine("dSPACE Programming Task Solution:");
+
+            signalDecoder = new SignalDecoder(@"Resources/InputFile.txt");
         }
     }
 }
---
+++ dSPACE.Programming.Task/dSPACE.Programming.Task/SignalDecoder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace dSPACE.Programming.Task
+{
+    public class SignalDecoder : IDisposable
+    {
+        private FileStream stream;
+        private StreamReader reader;        
+
+        public SignalDecoder()
+        {
+            stream = null;
+            reader = null;
+        }
+
+        public SignalDecoder(string path) : this()
+        {
+            Open(path);
+        }
+
+        ~SignalDecoder()
+        {
+            Close();
+        }
+
+        public void Open(string path)
+        {
+            // Make sure we are in a valid state
+            Close();
+
+            /*
+             * Copy this Resource folder in dSPACE.Programming.Task\bin\Debug\netcoreapp3.1 folder
+             * or copy fullpath from Resources folder and set path value.
+             * For example: 
+             *  path = E:\dspace_prog_test\dSPACE.Programming.Task\dSPACE.Programming.Task\Resources\InputFile.txt;
+             */
+
+
+            // Open a stream
+            stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            reader = new StreamReader(stream, Encoding.UTF8);
+
+            string line = String.Empty;            
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+                       
+        }        
+
+        public void Close()
+        {
+            stream?.Close();
+            stream = null;
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
