c6f74c31e813e5414aadd03c17e8ffac3b169f70
RizwanRumi <rizwan.cse33@gmail.com>
2023-10-14 14:37:15 +0200

    add PduData class

---
+++ dSPACE.Programming.Task/dSPACE.Programming.Task/PduData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dSPACE.Programming.Task
+{
+    public class PduData
+    {
+        public string PDUId { get; set; }
+        public string PayloadData { get; set;}
+        public ProtocolData ProtocolData { get; set; }
+        
+    }
+}
